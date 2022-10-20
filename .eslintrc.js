540a06e6417d54f6e8abb071dcd0878777502cd8
luankefei <luankefei@gmail.com>
2022-10-20 13:54:41 +0800

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,48 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
-  overrides: [],
+  extends: [
+    "standard-with-typescript",
+    "plugin:react/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+      ],
+      rules: {
+        "react/no-children-prop": "off",
+        "react/react-in-jsx-scope": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/explicit-function-return-type": "warn",
+      },
+      plugins: ["@typescript-eslint"],
+      parserOptions: {
+        warnOnUnsupportedTypeScriptVersion: true,
+        project: ["./tsconfig.json"], // Specify it only for TypeScript files
+      },
+    },
+    {
+      files: ["*.test.js"],
+      env: { jest: true },
+    },
+    {
+      files: [".eslintrc.js", "babel.config.js", "scripts/**/*.js"],
+      env: { node: true },
+    },
+    {
+      files: ["*.cjs"],
+      parserOptions: { sourceType: "script" },
+    },
+  ],
+
+  ignorePatterns: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
